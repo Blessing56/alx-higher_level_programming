efa12e0f8c47eaaecda1520455abb341e67b5aa9
Blessing <blessingnwalozie@gmail.com>
2022-08-23 15:23:14 -0700

    updating

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for i in range(00, 99):
-    print(i,' ',sep=',',end="")
+    if i < 10:
+        i = '0' + str(i)
+        print('{}'.format(i), end=", ")
+    else:
+        print('{}'.format(i), end=", ")
 print('\n')
