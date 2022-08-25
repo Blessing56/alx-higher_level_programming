7db802dd3ab7d1f7bd5e5b41756be32ec65f30c8
Blessing <blessingnwalozie@gmail.com>
2022-08-25 14:43:08 -0700

    updating

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def args(arg):
-    print('{} arguments:'.format(len(arg)))
+if __name__ == "__main__":
+    from sys import argv
+    args = argv[1:]
+
+    print('{}'.format(len(args)))
+
     for i in range(len(arg)):
-        print('{}: {}'.format((i + 1), arg[i]))
+            print('{}:{}'.format(i+1, arg[i]))
