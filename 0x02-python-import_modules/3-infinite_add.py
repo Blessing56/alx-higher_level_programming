a112a16a74c8570b67409351a1f11fb46049a034
Blessing <blessingnwalozie@gmail.com>
2022-08-26 09:12:02 -0700

    updating

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "m__main__":
+   from sys import argv
+
+    toatl = 0
+    for i in range(len(argv[1:] - 1):
+        total += int(argv[i])
+    print(('{}'.format(total))
+
