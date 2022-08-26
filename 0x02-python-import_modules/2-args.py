9601c23d69fd63850993752f2ee159fb198c5a65
Blessing <blessingnwalozie@gmail.com>
2022-08-26 09:20:48 -0700

    updating

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    args = argv[1:]
+    count = len(argv) - 1
 
-    print('{} '.format(len(args), end=""))
-
-    if len(args) == 0:
-        print('arguments.')
+    if count == 0:
+        print('0 arguments.')
+    elif count == 1:
+        print('1 arguments:')
     else:
-        print('arguments:')
+        print('{} arguments:'.format(count))
 
-    for i in range(len(arg)):
-            print('{}: {}'.format(i+1, arg[i]))
+    for i in range(count):
+        print('{}: {}'.format(i+1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "m__main__":
-   from sys import argv
-
-    toatl = 0
-    for i in range(len(argv[1:] - 1):
+    from sys import argv
+    total = 0
+    for i in range(len(argv[1:] - 1)):
         total += int(argv[i])
-    print(('{}'.format(total))
-
+    print('{}'.format(total))
