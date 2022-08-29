7fe969575f7a60b8dd1638d272db158db8abae97
Blessing <blessingnwalozie@gmail.com>
2022-08-29 11:54:56 -0700

    Adding a file

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_lst = []
+    for i in range(len(my_list)):
+        my_lst = my_list[(len(my_list) - 1) - i)
+        print('{:d}'.format(my_lst))
