e1a379b7aa2778cb941d9dd31531f05dedebc2df
Blessing56 <blessingnwalozie@gmail.com>
2022-08-31 23:55:19 +0000

    Adding files

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    lst = my_list[:]
     for i in range(len(my_list) - 1):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+        if lst[i] == search:
+            lst[i] = replace
+    return lst
