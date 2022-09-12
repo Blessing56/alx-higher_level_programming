49525177123a2e7562621320cdedc8223c15af87
Blessing <blessingnwalozie@gmail.com>
2022-09-12 12:11:13 -0700

    Created a new directory,added a README and another file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list = [], x = 0):
+    
+    count = 0
+    try:
+        for item in my_list:
+            if count < x:
+                print(f'{item}', end = '')
+                count = count + 1
+        print()
+        return count
+    except Indexerror:
+        print(my_list)
