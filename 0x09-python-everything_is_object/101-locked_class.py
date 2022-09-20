6aa7923f0caa59d37786d3ad77514916d2e04af6
Blessing56 <blessingnwalozie@gmail.com>
2022-09-20 15:59:18 +0000

    using __slot__ method

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,8 +5,10 @@
 
 class LockedClass:
     """
-    Preventing the user fron dynamically creating new instance attributes, except if the new instance atttribute is called first_name
+    Preventing the user fron dynamically creating new instance attributes,
+    except if the new instance atttribute is called first_name
     """
     __slots__ = ["first_name"]
+
     def __init__(self):
         pass
