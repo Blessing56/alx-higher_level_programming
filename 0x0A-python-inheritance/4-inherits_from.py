a3402cfe3bcd43266617182753e75554214a412c
Blessing56 <blessingnwalozie@gmail.com>
2022-09-27 01:42:33 +0000

    isinstance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""defining a class method"""
+"""defining an inherited class method"""
 
 
 def inherits_from(obj, a_class):
-    """Initializing a method"""
-    if isinstance(obj, a_class):
+    """
+    Checks if the object is  an inherited instance of the class
+
+    Args:
+        obj: Object tobe checked
+        a_class: the class type to be compared with the object
+    Returns:
+        True: is the object is an inherited class
+        False: otherwise
+    """
+    
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
