62ee46410da1dcb66ffe3086c9178ce7d8bf271a
Blessing56 <blessingnwalozie@gmail.com>
2022-09-27 03:00:42 +0000

    adding file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -37,6 +37,7 @@ class Rectangle(BaseGeometry):
 
     """
     def __init__(self, width, height):
+        """calling the integer_validate method to validate the integer"""
         super().integer_validator("width", width)
         super().integer_validator('height', height)
         self.__width = width
