c420530c9c8651d4b571e85cf48b5084e6e6beaa
Blessing56 <blessingnwalozie@gmail.com>
2022-09-27 01:30:47 +0000

    isinstance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defining a class method"""
 
+
 def is_kind_of_class(obj, a_class):
     """checking if the object is an instance of the class"""
     if isinstance(obj, a_class):
