ff414d09bf27e0150ce3f5d1a31c2680cef770a0
Blessing56 <blessingnwalozie@gmail.com>
2022-09-27 03:08:17 +0000

    adding file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,32 +1,8 @@
 #!/usr/bin/python3
-"""Defining a class BaseGeometry"""
-
-
-class BaseGeometry:
-    """definfig a class method"""
-
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validates a parameter as an integer
-
-        Args:
-            name: a string attribute
-            value: value attribute
-        Raises:
-            TypeError: If value not an integer
-            ValueError: If value is <= 0
-        """
-
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
 """Rectangle inherites the class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Instantaiating the class Rectangle
 
@@ -37,8 +13,10 @@ class Rectangle(BaseGeometry):
 
     """
     def __init__(self, width, height):
-        """calling the integer_validate method to validate the integer"""
-        self.integer_validator("width", width)
+        """calling the integer_validate method 
+        to validate the integer
+        """
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator('height', height)
+        super().integer_validator('height', height)
         self.__height = height
