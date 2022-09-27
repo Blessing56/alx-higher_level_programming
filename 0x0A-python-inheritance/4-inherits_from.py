7ad483db1315cf0c2ea67d9328b63567fcdf466f
Blessing56 <blessingnwalozie@gmail.com>
2022-09-27 01:34:58 +0000

    isinstance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defining a class method"""
+
+
+def inherits_from(obj, a_class):
+    """Initializing a method"""
+    if isinstance(obj, a_class):
+        return True
+    return False
