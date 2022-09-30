f7d725c772263a235b005a25d5753a6a013031b3
Blessing56 <blessingnwalozie@gmail.com>
2022-09-30 21:48:24 +0000

    Adding files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a file  that reads text"""
+
+
+def read_file(filename=""):
+   """Prints the contents of the file to stdout"""
+   with open(filename, encoding="utf-8") as d:
+        print(d.read(), end='')
