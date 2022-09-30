48ce40eec6fe7522a06849dc7585f4e339c8450d
Blessing56 <blessingnwalozie@gmail.com>
2022-09-30 22:01:47 +0000

    writing to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that writes a string to a textfile"""
+
+
+def write_file(filename="", text=""):
+    """Returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as d:
+        print(d.write(text))
