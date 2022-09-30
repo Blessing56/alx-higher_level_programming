a172a0e0a8ba922f259651470073f8ba79033e28
Blessing56 <blessingnwalozie@gmail.com>
2022-09-30 22:22:00 +0000

    JSOn representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a JSON function"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object
+    Args:
+        my_obj(str): string object
+    Returns:
+        The representation of the object
+    """
+
+    json.dumps(my_obj)
