d0572cbb069abd86ce751c3ecd701c3ea7677fe6
Blessing56 <blessingnwalozie@gmail.com>
2022-10-04 14:09:16 +0000

    JSON representation of an object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,7 +4,7 @@ import json
 
 
 def from_json_string(my_str):
-    """Returns an object (python data structure) represented 
+    """Returns an object (python data structure) represented
     by a JSON string"""
 
     return json.loads(my_str)
