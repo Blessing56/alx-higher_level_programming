7991b80a27a0db4e18c07b55fa6eaa285b994bef
Blessing56 <blessingnwalozie@gmail.com>
2022-11-03 12:39:30 +0000

    Adding files

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name varchar(256)
 	score int
 );
-INSERT INTO second_table (1, "John", 10);
-INSERT INTO second_table (2, "Alex", 3);
-INSERT INTO second_table (3, "Bob", 14);
-INSERT INTO second_table (4, "George", 8);
+INSERT INTO second_table
+VALUES 	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
