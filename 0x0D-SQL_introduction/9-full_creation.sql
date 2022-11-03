c240cae2af6239f0d5682aafca0ab71f7ece3d0f
Blessing56 <blessingnwalozie@gmail.com>
2022-11-03 12:33:05 +0000

    Adding files

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- second_table description:id INT, name VARCHAR(256), score INT
+--If the table second_table already exists, your script should not fail
+--You are not allowed to use the SELECT and SHOW statements
+
+CREATE TABLE second_table (
+	id int.
+	name varchar(256)
+	score int
+);
+INSERT INTO second_table (1, "John", 10);
+INSERT INTO second_table (2, "Alex", 3);
+INSERT INTO second_table (3, "Bob", 14);
+INSERT INTO second_table (4, "George", 8);
