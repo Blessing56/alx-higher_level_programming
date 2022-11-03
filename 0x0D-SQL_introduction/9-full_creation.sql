b29108e0d601f353c4688b05b4418787a356ce14
Blessing56 <blessingnwalozie@gmail.com>
2022-11-03 12:46:13 +0000

    Adding files

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,11 +3,7 @@
 --If the table second_table already exists, your script should not fail
 --You are not allowed to use the SELECT and SHOW statements
 
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
 VALUES 	(1, "John", 10),
 	(2, "Alex", 3),
