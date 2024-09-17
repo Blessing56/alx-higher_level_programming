d6de99cfa040d7f56ea14240682af2846eb61ca9
Blessing Nwalozie <blessingnwalozie@gmail.com>
2024-09-17 14:36:06 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4
+    import hidden_4 as hidden
 
-    names = dir(hidden_4)
-    for name in manes:
-        if name[:2] != "__":
-            print(name)
+    names = dir(hidden)
+   for x in range(len(list)):
+
+        if(list[x][0] != '_'):
+
+            print(list[x])
